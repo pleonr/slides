f1455fd441b4c31cbd4732240029a195288107c8
Pablo Leon Rodrigues <pablo.leonrodrigues@gmail.com>
2024-03-17 23:40:57 -0300

    brief_history_web

---
+++ dist/assets/slidev/12-NSBdo81Y.js
@@ -0,0 +1,18 @@
+import{_ as l}from"./CodeBlockWrapper-DvDszKMr.js";import{o as h,c as r,k as e,l as k,m as p,q as c,s as o,A as a,e as s,$ as i}from"../modules/vue-DqM6j7Au.js";import{I as d}from"./default-C9brjiYi.js";import{b as A,P as n}from"../index-CydXal7A.js";import{p as E,u as g,f as D}from"./context-CiTY-3FF.js";import"../modules/unplugin-icons-DHJ9mzV3.js";import"../modules/shiki-BjLwEkPr.js";const m=s("h3",null,"Response Headers",-1),C=s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-html"},[s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#DBD7CAEE","--shiki-light":"#393A34"}},"Accept-Ranges: bytes")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#DBD7CAEE","--shiki-light":"#393A34"}},"Access-Control-Allow-Origin: *")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#DBD7CAEE","--shiki-light":"#393A34"}},"Age: 0")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#DBD7CAEE","--shiki-light":"#393A34"}},"Cache-Control: max-age=600")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#DBD7CAEE","--shiki-light":"#393A34"}},"Content-Encoding: gzip")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#DBD7CAEE","--shiki-light":"#393A34"}},"Content-Length: 3553")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#DBD7CAEE","--shiki-light":"#393A34"}},"Content-Type: text/html; charset=utf-8")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#DBD7CAEE","--shiki-light":"#393A34"}},"Date: Tue, 13 Feb 2024 22:30:23 GMT")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#DBD7CAEE","--shiki-light":"#393A34"}},"Expires: Tue, 13 Feb 2024 22:40:23 GMT")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#DBD7CAEE","--shiki-light":"#393A34"}},"Last-Modified: Sun, 04 Feb 2024 13:52:33 GMT")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#DBD7CAEE","--shiki-light":"#393A34"}},"Server: GitHub.com")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#DBD7CAEE","--shiki-light":"#393A34"}},"Vary: Accept-Encoding")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#DBD7CAEE","--shiki-light":"#393A34"}},"X-Fastly-Request-Id: ")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#DBD7CAEE","--shiki-light":"#393A34"}},"    9faddf5d2c8006c085438f7c68a7cbf80a3d4fe7")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#DBD7CAEE","--shiki-light":"#393A34"}},"X-Github-Request-Id: ")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#DBD7CAEE","--shiki-light":"#393A34"}},"    CF3A:65E93:116AA8:142208:65CB93E8")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#DBD7CAEE","--shiki-light":"#393A34"}},"X-Served-By: cache-gru-sbgr1930075-GRU")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#DBD7CAEE","--shiki-light":"#393A34"}},"X-Timer: S1707863423.250750,VS0,VE143")])])],-1),f={__name:"12",setup(_){return E(n),g(),(y,u)=>{const t=l;return h(),r(d,c(o(a(D)(a(n),11))),{default:e(()=>[m,k(t,p({},{ranges:["all"]}),{default:e(()=>[C]),_:1},16)]),_:1},16)}}},R=A(f,[["__file","/@slidev/slides/12.md"]]);export{R as default};
